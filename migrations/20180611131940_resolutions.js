983b8e56ecc26676950c20a5ac19ef163a97e40c
KJK <KJK@KJK.local>
2018-06-14 10:15:42 -0600

    update

--- migrations/20180611131940_resolutions.js
+++ migrations/20180611131940_resolutions.js
@@ -7,5 +7,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("resolutions");
+  return knex.schema.dropTableIfExists("resolution");
 };
